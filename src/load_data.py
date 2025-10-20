510fe89244503ff7b92c38e03b80560b7fe559e1
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-10-20 22:11:47 +0200

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,2 +1,18 @@
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
 
+def load_data():
  
+    transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+ 
+    train_data = datasets.ImageFolder(root='train',transform=transform)
+    test_data = datasets.ImageFolder(root='test',transform=transform)
+ 
+    train_loader = DataLoader(train_data,batch_size=16,shuffle=True)
+    test_loader = DataLoader(test_data,batch_size=16,shuffle=False)
+    
+    return train_data,test_data,train_loader,test_loader
