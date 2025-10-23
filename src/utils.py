4caaee52fd91693cab4a661efc543b5ab00ad145
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-10-23 20:00:32 +0200

    Add imshow function to display images with labels

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
 
+def imshow(images, labels):
+    plt.figure(figsize=(10, 10))
+    for i in range(5):
+        plt.subplot(1, 5, i + 1)
+        # Convert from (C, H, W) to (H, W, C)
+        plt.imshow(images[i].permute(1, 2, 0))  
+        plt.title(labels[i].item())  
+        plt.axis('off')
+    plt.show()
