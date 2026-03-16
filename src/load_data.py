2c1f5815865b1385143526fa670587dc6eb4629d
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-03-16 04:03:30 +0200

    Update load_data.py

--- src/load_data.py
+++ src/load_data.py
@@ -1,18 +1,37 @@
-from torchvision import datasets,transforms
-from torch.utils.data import DataLoader 
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+def get_dataloaders(data_dir, batch_size = 16, train_ratio = 0.8, seed = 42):
  
-def load_data():  
+ # create transfroms
+ train_transform = transforms.Compose([
+  transforms.Resize([224,224]),
+  transforms.RandomHorizontalFlip(),
+  transforms.ToTensor()
+ ])
+ test_transform = transforms.Compose([
+  transforms.Resize([224,224]),
+  transforms.ToTensor()
+ ]) 
  
-    transform = transforms.Compose([
-    transforms.Resize((224,224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+ # loading the full dataset
+ full_dataset = datasets.ImageFolder(data_dir)
+
+ # creating the splits
+ train_split = int(train_ratio * len(full_dataset))
+ test_split = len(full_dataset) - train_split
+
+ train_data, test_data = random_split(full_dataset, [train_split, test_split])
  
-    train_data = datasets.ImageFolder(root='train',transform=transform)
-    test_data = datasets.ImageFolder(root='test',transform=transform)
+ #applying transforms
+ train_data.dataset.transform = train_transform
+ test_data.dataset.transform = test_transform
  
-    train_loader = DataLoader(train_data,batch_size=16,shuffle=True)
-    test_loader = DataLoader(test_data,batch_size=16,shuffle=False)
-    
-    return train_data,test_data,train_loader,test_loader
+ # creating data loaders
+ train_loader = DataLoader(train_data, batch_size = batch_size, shuffle = True)
+ test_loader = DataLoader(test_data, batch_size = batch_size, shuffle = False)
+
+ return train_loader, test_loader
+
+
+ 
