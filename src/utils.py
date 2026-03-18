cc0a1d86e1d1c0c4f996072d88b1c7deae94ef92
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-03-18 06:18:13 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 
-def imshow(images, labels):
+def show_imaages(images, labels):
     plt.figure(figsize=(10, 10))
     for i in range(5):
         plt.subplot(1, 5, i + 1)
         # Convert from (C, H, W) to (H, W, C)
         plt.imshow(images[i].permute(1, 2, 0))  
-        plt.title(labels[i].item())  
+        plt.title('Cat' if labels[i] == 0 else 'Dog')  
         plt.axis('off')
     plt.show()
