21b48bdc17716ec12eb9d8af294217de66a75ec5
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-03-19 02:42:10 +0200

    Update show_images function to take n_images parameter
    Modified show_images function to accept a dynamic number of images to display.

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-def show_imaages(images, labels):
+def show_imaages(images, labels, n_images):
     plt.figure(figsize=(10, 10))
-    for i in range(5):
-        plt.subplot(1, 5, i + 1)
+    for i in range(n_images):
+        plt.subplot(1, n_images, i + 1)
         # Convert from (C, H, W) to (H, W, C)
         plt.imshow(images[i].permute(1, 2, 0))  
         plt.title('Cat' if labels[i] == 0 else 'Dog')  
